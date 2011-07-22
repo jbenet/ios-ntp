c178e952cb1549ccf8a4289bcbcfd4f566e75c02
Juan Batiz-Benet <jbenet@cs.stanford.edu>
2011-07-22 06:59:17 -0700

    Added the ios-ntp static framework target

---
+++ src/ios-ntp.h
@@ -0,0 +1,10 @@
+/*╔══════════════════════════════════════════════════════════════════════════════════════════════════╗
+  ║ NetworkClock.h                                                                                   ║
+  ║                                                                                                  ║
+  ║ Created by Gavin Eadie on Oct17/10                                                               ║
+  ║ Copyright 2010 Ramsay Consulting. All rights reserved.                                           ║
+  ╚══════════════════════════════════════════════════════════════════════════════════════════════════╝*/
+
+#import "NetAssociation.h"
+#import "NetworkClock.h"
+
