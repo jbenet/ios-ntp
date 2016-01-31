a30659ca9f495c8ddbd88997dab44687e7932a78
Gavin Eadie <gavin@umich.edu>
2016-01-31 14:39:35 -0500

    Strip out unused parts of conditional debug logging ..

--- ios-ntp-lib/ntp-log.h
+++ ios-ntp-lib/ntp-log.h
@@ -1,10 +1,4 @@
-#import <Availability.h>
-#import <Foundation/Foundation.h>
-
 #define NTP_Logging(fmt, ...)
-#define LogInProduction(fmt, ...) \
-    NSLog((@"%@|" fmt), [NSString stringWithFormat: @"%16s", \
-        [[[self class] description] UTF8String]], ##__VA_ARGS__)
 
 #ifdef IOS_NTP_LOGGING
     #undef NTP_Logging
